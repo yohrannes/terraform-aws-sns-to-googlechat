afad400a4965ecb21548c7bf9e2b3ba975e69685
yohrannes <yohrannes@gmail.com>
2025-08-13 18:31:24 -0300

    update

--- lambda.tf
+++ lambda.tf
@@ -1,46 +1,8 @@
 data "aws_region" "current" {}
-data "archive_file" "lambda_zip" {
-  type        = "zip"
-  output_path = "${var.alert_name}.zip"
-  source_dir  = "${path.root}/lambda_function"
-}
 
-resource "aws_iam_role" "lambda_alert_role" {
-  name = "${var.alert_name}-alert-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      }
-    ]
-  })
-
-  tags = {
-    Name = "${var.alert_name}alert-role"
-  }
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
-  role       = aws_iam_role.lambda_alert_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_lambda_function" "alert_function" {
-  filename         = "${var.alert_name}.zip"
-  function_name    = "function_${var.alert_name}"
-  role            = aws_iam_role.lambda_alert_role.arn
-  handler         = "lambda_function.lambda_handler"
-  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
-  runtime         = "python3.9"
-  timeout         = 60
-
-provisioner "local-exec" {
+# Primeiro cria o diretório e os arquivos
+resource "null_resource" "create_lambda_files" {
+  provisioner "local-exec" {
     command = <<-EOT
       mkdir -p ${path.root}/lambda_function
       cd ${path.root}/lambda_function
@@ -262,6 +224,51 @@ def lambda_handler(event, context):
 EOF
     EOT
   }
+}
+
+# Depois cria o zip, dependendo dos arquivos criados
+data "archive_file" "lambda_zip" {
+  depends_on = [null_resource.create_lambda_files]
+  
+  type        = "zip"
+  output_path = "${var.alert_name}.zip"
+  source_dir  = "${path.root}/lambda_function"
+}
+
+resource "aws_iam_role" "lambda_alert_role" {
+  name = "${var.alert_name}-alert-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.alert_name}alert-role"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_execution" {
+  role       = aws_iam_role.lambda_alert_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "alert_function" {
+  filename         = "${var.alert_name}.zip"
+  function_name    = "function_${var.alert_name}"
+  role            = aws_iam_role.lambda_alert_role.arn
+  handler         = "lambda_function.lambda_handler"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
+  runtime         = "python3.9"
+  timeout         = 60
 
   environment {
     variables = {
